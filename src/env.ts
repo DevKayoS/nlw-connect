e1078103da13308c21fe1f07d469c3486294c5a6
DevKayoS <kayoribeirodev@gmail.com>
2025-02-19 20:53:42 -0300

    feat: ajustando a identacao

--- src/env.ts
+++ src/env.ts
@@ -2,9 +2,9 @@ import z from 'zod'
 import 'dotenv/config'
 
 const envSchema = z.object({
-  PORT: z.coerce.number().default(3333),
-  POSTGRES_URL: z.string().url(),
-  REDIS_URL: z.string().url(),
+    PORT: z.coerce.number().default(3333),
+    POSTGRES_URL: z.string().url(),
+    REDIS_URL: z.string().url(),
 })
 
 export const env = envSchema.parse(process.env)
--- src/routes/subscribe-to-event-route.ts
+++ src/routes/subscribe-to-event-route.ts
@@ -2,31 +2,31 @@ import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import z from 'zod'
 
 export const subscriberToEventRoute: FastifyPluginAsyncZod = async app => {
-  app.post(
-    '/subscribers',
-    {
-      schema: {
-        summary: 'Subscribes someone to the event',
-        tags: ['Subscriton'],
-        body: z.object({
-          name: z.string(),
-          email: z.string().email(),
-        }),
-        response: {
-          201: z.object({
-            name: z.string(),
-            email: z.string(),
-          }),
+    app.post(
+        '/subscribers',
+        {
+            schema: {
+                summary: 'Subscribes someone to the event',
+                tags: ['Subscriton'],
+                body: z.object({
+                    name: z.string(),
+                    email: z.string().email(),
+                }),
+                response: {
+                    201: z.object({
+                        name: z.string(),
+                        email: z.string(),
+                    }),
+                },
+            },
         },
-      },
-    },
-    async (request, reply) => {
-      const { name, email } = request.body
+        async (request, reply) => {
+            const { name, email } = request.body
 
-      return reply.status(201).send({
-        name,
-        email,
-      })
-    }
-  )
+            return reply.status(201).send({
+                name,
+                email,
+            })
+        }
+    )
 }
