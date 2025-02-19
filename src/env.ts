0116f483a19f1f8a6896950277c4d604fff9bc6f
DevKayoS <kayoribeirodev@gmail.com>
2025-02-19 00:20:35 -0300

    feat: criando configuracao inicial com o postgres

--- src/env.ts
+++ src/env.ts
@@ -3,6 +3,8 @@ import 'dotenv/config'
 
 const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
+  POSTGRES_URL: z.string().url(),
+  REDIS_URL: z.string().url(),
 })
 
 export const env = envSchema.parse(process.env)
