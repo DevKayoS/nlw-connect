3c244205f55dabcccfc5961490512cb691b1ee66
DevKayoS <kayoribeirodev@gmail.com>
2025-02-19 21:55:37 -0300

    feat: criando as migrates

--- src/drizzle/schema/subscriptions.ts
+++ src/drizzle/schema/subscriptions.ts
@@ -1,7 +1,8 @@
-import { pgTable, text, uuid } from 'drizzle-orm/pg-core'
+import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core'
 
 export const subscriptions = pgTable('subscriptions', {
     id: uuid('id').primaryKey().defaultRandom(),
     name: text('name').notNull(),
     email: text('email').notNull().unique(),
+    createdAt: timestamp('created_at').notNull().defaultNow(),
 })
